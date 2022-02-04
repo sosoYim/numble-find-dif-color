4c4bc04eb2f6b39cdd387971b98c9c45a8d3a7c4
sosoYim <sohyung.yim@gmail.com>
2022-02-04 15:04:47 +0900

    feat: Use useReducer instead of useState
    - create defaultProps
    - Not yet seperated as file

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,19 +1,55 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import './App.css';
 import { Board } from 'components';
 
+type State = {
+  stage: number;
+  score: number;
+  leftTime: number;
+};
+
+type Action =
+  | { type: 'INITIALIZE_GAME'; defaultProps: State }
+  | { type: 'NEXT_STAGE' }
+  | { type: 'DECREASE_LEFTTIME'; leftTime: number };
+
+function reducer(state: State, action: Action): State {
+  switch (action.type) {
+    case 'INITIALIZE_GAME':
+      return {
+        ...action.defaultProps,
+      };
+    case 'NEXT_STAGE':
+      return {
+        ...state,
+        stage: (state.stage += 1),
+      };
+    case 'DECREASE_LEFTTIME':
+      return {
+        ...state,
+        leftTime: state.leftTime - action.leftTime,
+      };
+    default:
+      throw new Error('Wrong action type');
+  }
+}
+
+const defaultProps: State = {
+  stage: 1,
+  score: 0,
+  leftTime: 15,
+};
+
 export function App() {
-  const [stage, setStage] = useState(1);
-  // TODO: 최초 시간 상수화
-  const [leftTime, setLeftTime] = useState(15);
-  const [score, setScore] = useState(0);
+  const [state, dispatch] = useReducer(reducer, defaultProps);
 
   return (
     <>
       <header>
-        스테이지: {stage}, 남은 시간: {leftTime}, 점수: {score}
+        스테이지: {state.stage}, 남은 시간: {state.leftTime}, 점수:{' '}
+        {state.score}
       </header>
-      <Board stage={stage} />
+      <Board stage={state.stage} />
     </>
   );
 }
