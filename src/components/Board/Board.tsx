3dd6f50b83672ca74246779d3bf72e245ea5b828
sosoYim <sohyung.yim@gmail.com>
2022-02-05 13:14:49 +0900

    fix: Delete data-idx, compare children idx
    - To find answer, compare target, currenttarget.children
    - Delete dataset

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -13,13 +13,10 @@ export function Board({ stage, handleClick }: BoardProps) {
     const tempAnswerIndex = getRandomInt(piecesCount as number, 0);
     setAnswerIndex(tempAnswerIndex);
     const [normalRGBCode, answerRGBCode] = getRandomColorAndAnswerColor(stage);
-    console.log({ stage, piecesCount, tempAnswerIndex });
-    // 생성한 piece 갯수와 정답 idx
     setPieces(
       Array.from({ length: piecesCount }, (_, i) => (
         <Piece
           key={i}
-          data-idx={i}
           backgroundColor={
             i === tempAnswerIndex ? answerRGBCode : normalRGBCode
           }
@@ -31,7 +28,12 @@ export function Board({ stage, handleClick }: BoardProps) {
   return (
     <div
       className="board"
-      onClick={(e: any) => handleClick(e, answerIndex == e.target.dataset.idx)}
+      onClick={(e: any) =>
+        handleClick(
+          e,
+          e.target === e.currentTarget.children[answerIndex as number],
+        )
+      }
     >
       {pieces}
     </div>
